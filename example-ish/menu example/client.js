b636d5af2b11f1f47a5a2f6b8b4be539305a94c8
Mitchel <mitchelvanhamburg@gmail.com>
2017-05-05 23:47:37 +0200

    Added 'examples' folder for random scripts we made we can look at for future reference, also updated .gitignore.

---
+++ example-ish/menu example/client.js
@@ -0,0 +1,52 @@
+﻿var mainMenu = null;
+
+API.onResourceStart.connect(function () {
+    createMenu();
+});
+
+API.onKeyDown.connect(function (sender, e) {
+    if (e.KeyCode === Keys.F2 && mainMenu.Visible === false) {
+        API.sendChatMessage("~g~Opened menu");
+        openMenu();
+    } else if (e.KeyCode === Keys.F2 && mainMenu.Visible === true) {
+        API.sendChatMessage("~r~Closed menu");
+        closeMenu();
+    }
+});
+
+API.onUpdate.connect(function () {
+    if (mainMenu != null) {
+        API.drawMenu(mainMenu)
+    }
+});
+
+function createMenu() {
+    mainMenu = API.createMenu("", "Vehicle menu", 0, 0, 6);
+
+    mainMenu.AddItem(API.createMenuItem("Repair vehicle", "Defuq you think it does?"));
+    mainMenu.AddItem(API.createMenuItem("Add all performance upgrades", "Engine upgrade, transmission mod, turbo etc.."));
+
+    mainMenu.Visible = false;
+    mainMenu.ResetKey(menuControl.Back);
+}
+
+function openMenu() {
+    mainMenu.Visible = true;
+    API.drawMenu(mainMenu);
+
+    mainMenu.OnItemSelect.connect(function (sender, item, index) {
+        switch (index) {
+            case 0:
+                API.triggerServerEvent("fixVehiclePlayer");
+                break;
+            case 1:
+                API.triggerServerEvent("modVehiclePerformance")
+                break;
+        }
+    });
+
+}
+
+function closeMenu() {
+    mainMenu.Visible = false;
+}
---
+++ example-ish/menu example/main.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using GTANetworkServer;
+using GTANetworkShared;
+
+class vehicle_options : Script
+{
+    public vehicle_options()
+    {
+        API.onResourceStart += init;
+        API.onClientEventTrigger += OnClientEvent;
+    }
+
+    public void init()
+    {
+        API.sendNotificationToAll("~g~Vehicle options started! press F2 to open the menu");
+    }
+
+    public void OnClientEvent(Client player, string eventName, params object[] arguments)
+    {
+        var inVehicle = API.isPlayerInAnyVehicle(player);
+        var getVehicle = API.getPlayerVehicle(player);
+        switch(eventName)
+        {
+            case "fixVehiclePlayer":
+                if (inVehicle)
+                {
+                    API.repairVehicle(getVehicle);
+                    API.sendNotificationToPlayer(player, "~g~Vehicle repaired");
+                } else
+                {
+                    API.sendNotificationToPlayer(player, "~r~You're not in a vehicle!");
+                }
+                break;
+            case "modVehiclePerformance":
+                if(inVehicle)
+                {
+                    API.setVehicleMod(getVehicle, 11, 3);
+                    API.setVehicleMod(getVehicle, 12, 3);
+                    API.setVehicleMod(getVehicle, 13, 3);
+                    API.setVehicleMod(getVehicle, 15, 3);
+                    API.setVehicleMod(getVehicle, 18, 1);
+                } else
+                {
+                    API.sendNotificationToPlayer(player, "~r~You're not in an vehicle!");
+                }
+                break;
+        }
+    }
+}
