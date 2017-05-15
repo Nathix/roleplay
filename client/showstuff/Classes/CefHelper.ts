298d5e17e58bdde912e83adc630e83a216399ac2
MatthewRorke <matthewrorke@gmail.com>
2017-05-15 22:15:10 +0200

    Added the phone

---
+++ client/showstuff/Classes/CefHelper.ts
@@ -0,0 +1,37 @@
+﻿/// <reference path="../types-gtanetwork/index.d.ts" />
+
+class CefHelper {
+
+    path: string;
+    open: boolean;
+    browser: GTANetwork.GUI.Browser;
+    
+    constructor (resourcePath) {
+        this.path = resourcePath
+        this.open = false
+    }
+
+    show () {
+        if (this.open === false) {
+        this.open = true
+
+        var resolution = API.getScreenResolution()
+
+        this.browser = API.createCefBrowser(resolution.Width, resolution.Height, true)
+        API.waitUntilCefBrowserInit(this.browser)
+        API.setCefBrowserPosition(this.browser, 0, 0)
+        API.loadPageCefBrowser(this.browser, this.path)
+        API.showCursor(true)
+        }
+    }
+
+    destroy () {
+        this.open = false
+        API.destroyCefBrowser(this.browser)
+        API.showCursor(false)
+    }
+
+    eval (string) {
+        this.browser.eval(string)
+    }
+}
---
+++ client/showstuff/main.ts
@@ -0,0 +1,13 @@
+﻿/// <reference path="types-gtanetwork/index.d.ts" />
+/// <reference path="Classes/CefHelper.ts"/>
+
+var cef = null;
+
+API.onResourceStart.connect(function () {
+    cef = new CefHelper('../../../www/phone/index.html');
+    cef.show();
+});
+
+API.onResourceStop.connect(function () {
+    cef.destroy();
+});
